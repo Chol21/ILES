f89d809aa4d51830f4f7bbb9534b3dcf9003808a
CHOL NHIAL AJAK <Cholnhial21@gmail.com>
2026-04-09 14:47:08 +0300

    Add API views and URL configuration

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets, permissions 
+from core.models import CustomUser, InternshipPlacement 
+from .serializers import CustomUserSerializer, InternshipPlacementSerializer 
+class CustomUserViewSet(viewsets.ModelViewSet): 
+    queryset = CustomUser.objects.all() 
+    serializer_class = CustomUserSerializer 
