9cd61a5913e854b984c4eea79d38c9efc8b4893d
CHOL NHIAL AJAK <Cholnhial21@gmail.com>
2026-06-16 15:30:06 +0300

    Fix docs routes by removing silent try/except

--- iles_project/urls.py
+++ iles_project/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-
-try:
-    from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerUIView
-    docs_urls = [
-        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-        path('api/docs/', SpectacularSwaggerUIView.as_view(url_name='schema'), name='swagger-ui'),
-    ]
-except ImportError:
-    docs_urls = []
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerUIView
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Your API endpoints (auth, users, students, placements, etc.)
     path('api/', include('api.urls')),
-] + docs_urls
+
+    # OpenAPI schema (JSON)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # Swagger UI (the pretty docs page)
+    path('api/docs/', SpectacularSwaggerUIView.as_view(url_name='schema'), name='swagger-ui'),
+]
